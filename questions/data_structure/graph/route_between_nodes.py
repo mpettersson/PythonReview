7266e67743efe7d1355e236d1de40caabff10cc3
Matthew Pettersson <matt@mpettersson.com>
2020-08-22 17:26:24 -0700

    More graph/tree questions.

--- questions/data_structure/graph/route_between_nodes.py
+++ questions/data_structure/graph/route_between_nodes.py
@@ -1,80 +1,107 @@
-"""
+r"""
     ROUTE BETWEEN NODES (CCI 4.1)
 
     Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
 
-    For example, given the following graph, node 6, and node 7, the algorithm will return False.  If the graph, node 1,
-    and node 0 were supplied, the algorithm would return True.
+    Consider the following graph (where each link represents a bi-directional link):
 
               1
             /   \
            3     2
          /  \
-        4----6   7---8---9
+        4----6   7---8
             / \
            5---0
 
-    The above graph as an adjacency matrix:
-
-        # 0  1  2  3  4  5  6  7  8  9
-        [[0, 0, 0, 0, 0, 1, 1, 0, 0, 0],  # 0
-         [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],  # 1
-         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],  # 2
-         [0, 1, 0, 0, 1, 0, 1, 0, 0, 0],  # 3
-         [0, 0, 0, 1, 0, 0, 1, 0, 0, 0],  # 4
-         [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],  # 5
-         [1, 0, 0, 1, 1, 1, 0, 0, 0, 0],  # 6
-         [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],  # 7
-         [0, 0, 0, 0, 0, 0, 0, 1, 0, 1],  # 8
-         [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]]  # 9
+    Example:
+        Input = graph, 0, 2   # where graph is the graph above
+        Output = True
 """
 
 
-def route_between_nodes_via_bfs(g, start_node, end_node):
-    g.clear_searched()
-    queue = [start_node]
-    start_node.searched = True
-    while len(queue) > 0:
-        curr_node = queue.pop(0)
-        if curr_node == end_node:
+# DFS Approach (w/ set of visited nodes): Time complexity is O(n + e), where n is the number of nodes and e is the
+# number of edges in the graph. Space complexity is O(n).
+def route_between_nodes_dfs(graph, start, end, visited_set=None):
+    if isinstance(graph, Graph) and isinstance(start, Node) and isinstance(end, Node):
+        if start is end:
             return True
-        for n in curr_node.adj:
-            if not n.searched:
-                queue.append(n)
-                n.searched = True
-    return False
+        s = visited_set if visited_set else set()
+        s.add(start)
+        for n in start.neighbors:
+            if n not in s:
+                if route_between_nodes_dfs(graph, n, end, s):
+                    return True
+        return False
 
 
-def route_between_nodes_via_dfs(g, start_node, end_node):
-    g.clear_searched()
-    return depth_first_search(start_node, end_node)
+# DFS Approach (w/ tracked state in the node): Time complexity is O(n + e), where n is the number of nodes and e is the
+# number of edges in the graph. Space complexity is O(n).
+def route_between_graph_nodes_dfs(graph, start, end):
+    def _dfs(start, end):
+        if start == end:
+            return True
+        if not start.searched:
+            start.searched = True
+            for n in start.neighbors:
+                if _dfs(n, end):
+                    return True
+        return False
+
+    if isinstance(graph, Graph) and isinstance(start, Node) and isinstance(end, Node):
+        graph.clear_searched()
+        return _dfs(start, end)
 
 
-def depth_first_search(curr_node, target_node):
-    if curr_node == target_node:
-        return True
-    if not curr_node.searched:
-        curr_node.searched = True
-        for n in curr_node.adj:
-            if depth_first_search(n, target_node):
+# BFS Approach (w/ set of visited nodes): Time complexity is O(n + e), where n is the number of nodes and e is the
+# number of edges in the graph. Space complexity is O(n).
+def route_between_nodes_bfs(graph, node, dest):
+    if isinstance(graph, Graph) and isinstance(node, Node) and isinstance(dest, Node):
+        s = set()
+        s.add(node)
+        q = [node]
+        while len(q) > 0:
+            n = q.pop(0)
+            if n is dest:
                 return True
-    return False
+            for adj in n.neighbors:
+                if adj not in s:
+                    s.add(adj)
+                    q.append(adj)
+        return False
 
 
-class GNode:
-    def __init__(self, value, adj=None):
+# DFS Approach (w/ tracked state in the node): Time complexity is O(n + e), where n is the number of nodes and e is the
+# number of edges in the graph. Space complexity is O(n).
+def route_between_graph_nodes_bfs(graph, start, end):
+    if isinstance(graph, Graph) and isinstance(start, Node) and isinstance(end, Node):
+        graph.clear_searched()
+        queue = [start]
+        start.searched = True
+        while len(queue) > 0:
+            curr_node = queue.pop(0)
+            if curr_node == end:
+                return True
+            for n in curr_node.neighbors:
+                if not n.searched:
+                    queue.append(n)
+                    n.searched = True
+        return False
+
+
+class Node:
+    def __init__(self, value, neighbors=None):
         self.value = value
-        self.adj = []
+        self.neighbors = []
         self.searched = False
-        if adj:
-            if type(adj) is list:
-                self.adj += adj
+        if neighbors:
+            if type(neighbors) is list:
+                self.neighbors += neighbors
             else:
-                self.adj.append(adj)
+                self.neighbors.append(neighbors)
 
     def add_adj(self, n):
-        if n not in self.adj:
-            self.adj.append(n)
+        if n not in self.neighbors:
+            self.neighbors.append(n)
 
     def __repr__(self):
         return repr(self.value)
@@ -87,53 +114,94 @@ class Graph:
             self.nodes = self.nodes + n if type(n) is list else self.nodes + [n]
 
     def __repr__(self):
-        nodes = []
-        for n in self.nodes:
-            nodes.append(repr(n))
-        return "[" + ", ".join(nodes) + "]"
+        return "[" + ', '.join(map(repr, self.nodes)) + "]"
 
-    def add_node(self, n):
-        if n in self.nodes:
-            raise ValueError
-        else:
-            self.nodes.append(n)
+    def __getitem__(self, item):
+        if 0 <= item < len(self.nodes):
+            return self.nodes[item]
 
-    def to_adj_list(self):
-        nodes = ""
-        for n in self.nodes:
-            nodes += f"{n.value}:{str(n.adj)}\n"
-        return nodes
+    def __iter__(self):
+        return iter(self.nodes)
+
+    def add_node(self, value, neighbors=None):
+        for node in self.nodes:
+            if node.value == value:
+                raise ValueError
+        self.nodes.append(Node(value, neighbors))
 
     def clear_searched(self):
         for n in self.nodes:
             n.searched = False
 
+    def to_adj_matrix(self):
+        v_dict = {i: k for i, k in enumerate(self.nodes)}
+        return [[1 if v_dict[i] in n.neighbors else 0 for i in range(len(self.nodes))] for n in self.nodes]
 
-nodes = []
+    def to_adj_list(self):
+        return {n.value: n.neighbors for n in self.nodes}
+
+    def populate_from_adj_matrix(self, adj_mat, values=None):
+        length = len(adj_mat)
+        if values and len(values) != length:
+            raise Exception("values must be None or have the same length as adj_mat")
+        if not values:
+            values = list(range(len(self.nodes), len(self.nodes) + length))  # Doesn't guarantee valid names/values.
+        for v in values:
+            self.add_node(v)
+        for i, l in enumerate(adj_mat):
+            for j, is_adj in enumerate(l):
+                if is_adj:
+                    self.nodes[-(length - i)].neighbors.append(self.nodes[-(length - j)])
+
+    def populate_from_adj_list(self, adj_list):
+        temp = {}
+        offset = len(self.nodes)
+        for i, k in enumerate(adj_list.keys()):
+            temp[k] = offset + i
+            self.add_node(k)
+        for k, v in adj_list.values():
+            for adj in v:
+                self.nodes[temp[k]].neighbors.append(self.nodes[temp[adj]])
+
+    def display_adj_matrix(self):
+        adj_mat = self.to_adj_matrix()
+        if not adj_mat:
+            print("None")
+        else:
+            print('\n'.join([''.join(['{:4}'.format(i) for i in r if len(r) > 0]) for r in adj_mat if len(adj_mat) > 0]))
+
+    def display_adj_list(self):
+        adj_list = self.to_adj_list()
+        if not adj_list:
+            print("None")
+        else:
+            [print(f"{k:4}: {v}") for k, v in adj_list.items()]
+
+
 graph = Graph()
+adj_mat = [[0, 0, 0, 0, 0, 1, 1, 0, 0], [0, 0, 1, 1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0],
+           [0, 1, 0, 0, 1, 0, 1, 0, 0], [0, 0, 0, 1, 0, 0, 1, 0, 0], [1, 0, 0, 0, 0, 0, 1, 0, 0],
+           [1, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 1, 0]]
+graph.populate_from_adj_matrix(adj_mat)
+args = [(graph[0], graph[2]),   # True
+        (graph[2], graph[0]),   # True
+        (graph[0], graph[7]),   # False
+        (graph[7], graph[0]),   # False
+        (graph[7], graph[8]),   # True
+        (graph[8], Node(9)),    # False
+        (graph[0], None),       # None
+        (None, graph[0]),       # None
+        (None, None)]           # None
 
-for i in range(10):
-    nodes.append(GNode(i))
-    graph.add_node(nodes[i])
+print("graph:")
+graph.display_adj_list()
+print()
 
-adjs = [[0, 0, 0, 0, 0, 1, 1, 0, 0, 0], [0, 0, 1, 1, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 1, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 1, 0, 0, 0], [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-        [1, 0, 0, 1, 1, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]]
+for source, dest in args:
+    print(f"route_between_nodes_dfs(graph, {source}, {dest})", route_between_nodes_dfs(graph, source, dest))
+print()
 
-for row in range(len(adjs)):
-    for col in range(len(adjs[0])):
-        if adjs[row][col]:
-            nodes[row].add_adj(nodes[col])
-
-print(graph)
-print(graph.to_adj_list())
-
-for row in range(len(adjs)):
-    for col in range(len(adjs[0])):
-        if row != col:
-            print(f"route_between_nodes_via_bfs(graph, {nodes[row].value}, {nodes[col].value})", route_between_nodes_via_bfs(graph, nodes[row], nodes[col]))
-            print(f"route_between_nodes_via_dfs(graph, {nodes[row].value}, {nodes[col].value})", route_between_nodes_via_dfs(graph, nodes[row], nodes[col]))
-
+for source, dest in args:
+    print(f"route_between_nodes_bfs(graph, {source}, {dest})", route_between_nodes_bfs(graph, source, dest))
 
 
---
+++ questions/data_structure/tree/first_common_ancestor_bst.py
@@ -0,0 +1,158 @@
+r"""
+    FIRST COMMON ANCESTOR BST
+
+    NOTE: If no BST ordering, or for trees with links to parents, SEE first_common_ancestor.py/"FIRST COMMON ANCESTOR".
+
+    Design an algorithm and write code to find the first common ancestor of two nodes in a binary search tree (BST).
+    Avoid storing additional nodes in a data structure.
+
+    Consider the following BST:
+
+              5
+            /   \
+           1     6
+         /  \
+        0    3
+            / \
+           2   4
+
+    Example:
+                my_bst = Node(5, Node(1, Node(4), Node(6, Node(5), Node(0))), Node(2))
+                node_4 = my_bst.left.left
+                node_5 = my_bst.left.right.left
+        Input = my_bst, node_4, node_5      # my_bst is the BST above
+        Output = 3                          # that is, the node with value three
+"""
+
+
+# Naive Two Path Approach:  This may, or may not, fulfil the interviewers space requirements, ask them.
+# Time complexity is O(n) and space complexity is O(h1 + h2), where h1 and h2 are the heights of n1 and n2 (however, the
+# worst case space could be O(n)).
+def first_common_ancestor_bst_naive(t, n1, n2):
+    if t and n1 and n2:
+        p1 = path_to_node(t, n1)
+        p2 = path_to_node(t, n2)
+        if p1 and p2:
+            if len(p2) > len(p1):
+                p1, p2 = p2, p1
+            while len(p1) > len(p2):
+                p1.pop(0)
+            while p1 and p1[0] != p2[0]:
+                p1.pop(0)
+                p2.pop(0)
+            if p1 and p1[0] == p2[0]:
+                return p1[0].value
+
+
+def path_to_node(root, node):
+    if root is node:
+        return [root]
+    if node.value < root.value and root.left:
+        l = path_to_node(root.left, node)
+        if l:
+            return l + [root]
+    if node.value > root.value and root.right:
+        l = path_to_node(root.right, node)
+        if l:
+            return l + [root]
+
+
+# Recursive Modified Find Approach: Using the BST property, recurse down from root,
+# Time complexity is O(n) where n is the number of nodes in the tree. Space complexity is O(h), where h is the height of
+# the tree ((however, the worst case space could be O(n)).
+def first_common_ancestor_bst_rec(root, n1, n2):
+    if root and n1 and n2:
+        if root is n1 and is_in_bst(root, n2) or \
+           root is n2 and is_in_bst(root, n1) or \
+           n1.value <= root.value < n2.value and is_in_bst(root.left, n1) and is_in_bst(root.right, n2) or \
+           n2.value <= root.value < n1.value and is_in_bst(root.left, n2) and is_in_bst(root.right, n1):
+            return root
+        elif n1.value <= root.value and n2.value <= root.value:
+            return first_common_ancestor_bst_rec(root.left, n1, n2)
+        elif n1.value > root.value and n2.value > root.value:
+            return first_common_ancestor_bst_rec(root.right, n1, n2)
+
+
+def is_in_bst(root, node):
+    if root and node:
+        if root is node:
+            return True
+        return True if root.right and node.value > root.value and is_in_bst(root.right, node) or \
+                       root.left and node.value <= root.value and is_in_bst(root.left, node) else False
+    return False
+
+
+class Node:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return repr(self.value)
+
+
+# Helper Function
+def display(node):
+    def wrapper(node):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        if node.right is None and node.left is None:                                        # No child.
+            return [str(node.value)], len(str(node.value)), 1, len(str(node.value)) // 2
+        if node.right is None:                                                              # Only left child.
+            lines, n, p, x = wrapper(node.left)
+            u = len(str(node.value))
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + str(node.value)
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+        if node.left is None:                                                               # Only right child.
+            lines, n, p, x = wrapper(node.right)
+            u = len(str(node.value))
+            first_line = str(node.value) + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+        else:                                                                               # Two children.
+            left, n, p, x = wrapper(node.left)
+            right, m, q, y = wrapper(node.right)
+            u = len(str(node.value))
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + str(node.value) + y * '_' + (m - y) * ' '
+            second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+            if p < q:
+                left += [n * ' '] * (q - p)
+            elif q < p:
+                right += [m * ' '] * (p - q)
+            lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zip(left, right)]
+            return lines, n + m + u, max(p, q) + 2, n + u // 2
+    if node:
+        lines, _, _, _ = wrapper(node)
+        for line in lines:
+            print(line)
+
+
+tree = Node(5, Node(1, Node(0), Node(3, Node(2), Node(4))), Node(6))
+args = [(tree.left.left, tree.left.right.left),                     # (0, 2)
+        (tree.left.right.left, tree.left.left),                     # (2, 0)
+        (tree.left, tree.right),                                    # (1, 6)
+        (tree.right, tree.left),                                    # (6, 1)
+        (tree.left.right.right, tree.right),                        # (4, 6)
+        (tree.left.right.left, tree.left.right.right),              # (2, 4)
+        (tree.left.right.left, Node(7)),                            # (2, 7) (7 isn't in the tree)
+        (tree.right, Node(8)),                                      # (6, 8) (8 isn't in the tree)
+        (Node(7), Node(8)),                                         # (7, 8) (7 and 8 isn't in the tree)
+        (None, Node(7)),                                            # (None, 7) (7 isn't in the tree)
+        (Node(7), None),                                            # (7, None) (7 isn't in the tree)
+        (None, None)]                                               # (None, None)
+
+print(f"display(tree):")
+display(tree)
+print()
+
+for n1, n2 in args:
+    print(f"first_common_ancestor_bst_naive(tree, {n1}, {n2}):", first_common_ancestor_bst_naive(tree, n1, n2))
+print()
+
+for n1, n2 in args:
+    print(f"first_common_ancestor_bst_rec(tree, {n1}, {n2}):", first_common_ancestor_bst_rec(tree, n1, n2))
+
+
