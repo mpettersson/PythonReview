a935c15b113b1138bd0f75952f977de6efacf017
Matthew Pettersson <matt@mpettersson.com>
2021-07-23 07:28:01 -0700

    Updated the matrix path product question.

--- questions/list/find_matrix_path_max_product.py
+++ questions/list/find_matrix_path_max_product.py
@@ -1,8 +1,8 @@
 """
     FIND MATRIX PATH MAX PRODUCT (50CIQ 3: MATRIX PRODUCT)
 
-    Write a function which accepts a matrix, then returns the maximum path values product, when starting at the top left
-    and ending at the bottom right of the matrix (with o
+    Write a function which accepts a matrix, then returns the maximum product of the path values, when starting at the
+    top left and ending at the bottom right of the matrix.
 
     Consider the following 2d lists:
         [[1, 2, 3],     [[-1, 2, 3],
@@ -15,78 +15,133 @@
 
         Input = [[-1, 2, 3], [4, 5, -6], [7, 8, 9]]
         Output = 1080  # or, -1 * 4 * 5 * -6 * 9
+
+    Variations:
+        - Use subtraction as the operator (not multiplication).
 """
+import time
 
 
-# Wrong Approach: This approach ONLY works if all values are positive; if there are some negative values such as the
-# second example above, then the result will be wrong. This is due to the order being reversed because of the recursion.
+# Questions you should ask the interviewer (if not explicitly stated):
+#   - CAN THE VALUES BE NEGATIVE?
+#   - What time/space complexity are you looking for?
+#   - Does the matrix consist of ragged or jagged lists?
+
+
+# (POSITIVE VALUES ONLY/WRONG IF NEG VALUES) APPROACH: Recursive
+#
+# This approach ONLY works if all values are POSITIVE; if there are some NEGATIVE values such as the second example
+# above, then the result will be wrong. This is due to the order being reversed because of the recursion.
+#
 # Time Complexity: O((r + c)!/(r! * c!)), where r and c are the number of rows and columns in the matrix.
 # Space Complexity: O(r + c), where r and c are the number of rows and columns in the matrix.
-def matrix_product_rec_wrong(m):
+def find_matrix_product_for_positive_values_only_rec(m):
 
-    def _matrix_product_rec_wrong(m, r, c):
+    def _rec(m, r, c):
         if r is len(m) - 1 and c is len(m[-1]) - 1:
             return m[r][c]
         if r + 1 is len(m):
-            return m[r][c] * _matrix_product_rec_wrong(m, r, c+1)
+            return m[r][c] * _rec(m, r, c+1)
         if c + 1 is len(m[r]):
-            return m[r][c] * _matrix_product_rec_wrong(m, r+1, c)
-        res_r = m[r][c] * _matrix_product_rec_wrong(m, r, c + 1)
-        res_d = m[r][c] * _matrix_product_rec_wrong(m, r + 1, c)
+            return m[r][c] * _rec(m, r+1, c)
+        res_r = m[r][c] * _rec(m, r, c + 1)
+        res_d = m[r][c] * _rec(m, r + 1, c)
         return max(res_r, res_d)
 
     if m and len(m) > 0 and len(m[0]) > 0:
-        return _matrix_product_rec_wrong(m, 0, 0)
+        return _rec(m, 0, 0)
 
 
-# Recursive Approach:  START at the END and recurse backwards to the starting location, returning either the minimum of
-# two negative values OR the maximum of two positive, or one positive and one negative value, along the way.
+# APPROACH: Recursive
+#
+# This is essentially the same as the wrong solution above, however, this solution handles the possibility of NEGATIVE
+# value.  This is accomplished by returning BOTH the minimum AND the maximum possible value for (or, if starting at)
+# EACH cell.
+#
+# NOTE: Either direction (starting at the end and working towards beginning or starting at the beginning and working to
+# the end) will work because we are using multiplication; we start at the end and work to the beginning to just check
+# for index 0 (not len(m)-1).
+#
 # Time Complexity: O((r + c)!/(r! * c!)), where r and c are the number of rows and columns in the matrix.
 # Space Complexity: O(r + c), where r and c are the number of rows and columns in the matrix.
-def matrix_product_rec(m):
+def find_matrix_product_rec(m):
 
-    def _matrix_product_rec(m, r, c):
-        if r is 0 and c is 0:
-            return m[r][c]
-        if r - 1 < 0:
-            return m[r][c] * _matrix_product_rec(m, r, c-1)
-        if c - 1 < 0:
-            return m[r][c] * _matrix_product_rec(m, r-1, c)
-        l_res = _matrix_product_rec(m, r, c-1)
-        u_res = _matrix_product_rec(m, r-1, c)
-        if l_res < 0 and u_res < 0:
-            return m[r][c] * min(l_res, u_res)
-        return m[r][c] * max(l_res, u_res)
+    def _rec(m, r, c):
+        if r == 0 and c == 0:                                   # Last position.
+            return m[r][c], m[r][c]
+        if r == 0:                                              # Can only recurse left.
+            res = _rec(m, r, c-1)
+            return m[r][c] * res[0], m[r][c] * res[1]
+        if c == 0:                                              # Can only recurse up.
+            res = _rec(m, r-1, c)
+            return m[r][c] * res[0], m[r][c] * res[1]
+        res_l = _rec(m, r, c - 1)                               # Recurse up and recurse left.
+        res_u = _rec(m, r - 1, c)
+        res = [m[r][c] * res_l[0], m[r][c] * res_l[1], m[r][c] * res_u[0], m[r][c] * res_u[1]]  # LMin, LMax, RMin, RMax
+        return min(res), max(res)
 
     if m and len(m) > 0 and len(m[0]) > 0:
-        return _matrix_product_rec(m, len(m)-1, len(m[-1])-1)
+        res_min, res_max = _rec(m, len(m)-1, len(m[-1])-1)
+        return res_max
 
 
-# Memoization/Dynamic Programming Approach:  This approach is similar to the regular recursive approach above, however,
-# a cache has been added to reduce duplicate recursive calls.
+# APPROACH: Top Down Dynamic Programming With Memoization
+#
+# This approach is similar to the plain recursive approach above, however, a cache (that maintains the max and min
+# values for each cell) has been added to reduce duplicate recursive calls.
+#
 # Time Complexity: O(r * c), where r and c are the number of rows and columns in the matrix.
 # Space Complexity: O(r * c), where r and c are the number of rows and columns in the matrix.
-def matrix_product_memo(m):
+def find_matrix_product_top_down_memo(m):
+
+    def _rec(m, r, c):
+        if r == 0 and c == 0:
+            return m[r][c], m[r][c]
+        if cache[r][c] is None:
+            if r == 0:
+                res = _rec(m, r, c-1)
+                cache[r][c] = m[r][c] * res[0], m[r][c] * res[1]
+            elif c == 0:
+                res = _rec(m, r-1, c)
+                cache[r][c] = m[r][c] * res[0], m[r][c] * res[1]
+            else:
+                res_l = _rec(m, r, c-1)
+                res_u = _rec(m, r-1, c)
+                res = [m[r][c] * res_l[0], m[r][c] * res_l[1], m[r][c] * res_u[0], m[r][c] * res_u[1]]
+                cache[r][c] = min(res), max(res)
+        return cache[r][c]
+
     if m and len(m) > 0 and len(m[0]) > 0:
-        memo = [[1 for _ in c] for c in m]
-        for r in range(len(m)):
-            for c in range(len(m[r])):
-                if r is 0 and c is 0:
-                    memo[r][c] = m[r][c]
-                elif r - 1 < 0:
-                    memo[r][c] = memo[r][c-1] * m[r][c]
-                elif c - 1 < 0:
-                    memo[r][c] = memo[r-1][c] * m[r][c]
-                else:
-                    if memo[r-1][c] < 0 and memo[r][c-1] < 0:
-                        memo[r][c] = m[r][c] * min(memo[r-1][c], memo[r][c-1])
-                    else:
-                        memo[r][c] = m[r][c] * max(memo[r-1][c], memo[r][c-1])
-        return memo[-1][-1]
+        cache = [[None] * len(m[0]) for _ in range(len(m))]
+        res_min, res_max = _rec(m, len(m)-1, len(m[-1])-1)
+        return res_max
+
+
+# APPROACH: Top Down Dynamic Programming With Memoization
+#
+# Fill a max and min cache starting at the top left and ending at the bottom right, then return the max cache's bottom
+# right value.
+#
+# Time Complexity: O(r * c), where r and c are the number of rows and columns in the matrix.
+# Space Complexity: O(r * c), where r and c are the number of rows and columns in the matrix.
+def find_matrix_product_bottom_up_memo(m):
+    min_cache = [[float('inf')] * len(m[0]) for _ in range(len(m))]
+    max_cache = [[-float('inf')] * len(m[0]) for _ in range(len(m))]
+    for r in range(len(m)):                                 # Iterate over Rows (No Recursion)
+        for c in range(len(m[0])):                              # Iterate over Columns (No Recursion)
+            if r == 0 and c == 0:                                   # Top Left
+                max_cache[r][c] = min_cache[r][c] = m[r][c]
+            if r > 0:                                               # All Non-First Rows
+                max_cache[r][c] = max(m[r][c] * max_cache[r-1][c], m[r][c] * min_cache[r-1][c], max_cache[r][c])
+                min_cache[r][c] = min(m[r][c] * max_cache[r-1][c], m[r][c] * min_cache[r-1][c], min_cache[r][c])
+            if c > 0:                                               # All Non-First Cols
+                max_cache[r][c] = max(m[r][c] * max_cache[r][c-1], m[r][c] * min_cache[r][c-1], max_cache[r][c])
+                min_cache[r][c] = min(m[r][c] * max_cache[r][c-1], m[r][c] * min_cache[r][c-1], min_cache[r][c])
+    return max_cache[-1][-1]
 
 
 def format_matrix(m):
-    return "\n" + '\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in m])
+    return "\n" + '\n'.join([''.join(['{:6}'.format(item) for item in row]) for row in m])
 
 
 matrices = [[[1, 2, 3],
@@ -95,6 +150,25 @@ matrices = [[[1, 2, 3],
             [[-1, 2, 3],
              [4, 5, -6],
              [7, 8, 9]],
+            [[9, -6, 3],
+             [8, 5, 2],
+             [7, 4, -1]],
+            [[-1, -2, -3],
+             [-2, -3, -3],
+             [-3, -3, -2]],
+            [[-3, 3, 3, 3, 3, 3, 3, 1, 1, -3],
+             [3, 3, 3, 3, 3, 3, 3, 1, 1, 1],
+             [3, 3, 3, 3, 3, 3, 3, 1, 1, 1],
+             [3, 3, 3, 3, 3, 3, 3, 1, 1, 1],
+             [3, 3, 3, 3, 3, 3, 3, 1, 1, 33333],
+             [3, 3, 3, 3, 3, 3, 3, 1, 1, 1],
+             [3, 3, 3, 3, 3, 3, 3, 1, 1, 1],
+             [3, 3, 3, 3, 3, 3, 3, 1, 1, 1],
+             [3, 3, 3, 3, 3, 3, 3, 1, 1, 1],
+             [3, 3, 3, 3, 3, 3, 3, 3, 3, -3]],
+            [[1, -2, 1],
+             [1, -2, 1],
+             [3, -4, 1]],
             [[1, 2, 3, -4, 5]],
             [[1, 2, 3, 0, 5]],
             [[1],
@@ -105,14 +179,22 @@ matrices = [[[1, 2, 3],
              [3]],
             [[1, 1],
              [0, 1]]]
-fns = [matrix_product_rec_wrong,
-       matrix_product_rec,
-       matrix_product_memo]
+fns = [find_matrix_product_rec,
+       find_matrix_product_top_down_memo,
+       find_matrix_product_bottom_up_memo]
 
 for matrix in matrices:
     print("matrix:", format_matrix(matrix))
     for fn in fns:
         print(f"{fn.__name__}(matrix): {fn(matrix)}")
     print()
+
+
+print("matrix:", format_matrix(matrices[4]))
+for fn in fns:
+    t = time.time()
+    print(f"{fn.__name__}(matrices[4]),", end="")
+    result = fn(matrices[4])
+    print(f" took {time.time() - t} seconds: {result}")
 
 
