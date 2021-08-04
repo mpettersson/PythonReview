486164ea0e41b82a16e20846ef12e508b572b82a
Matthew Pettersson <matt@mpettersson.com>
2021-08-04 08:08:45 -0700

    Updated the stacks_via_list problem.

--- questions/data_structure/stack/stacks_via_list.py
+++ questions/data_structure/stack/stacks_via_list.py
@@ -2,23 +2,37 @@
     STACKS VIA LIST (CCI 3.1: THREE IN ONE,
                      50CIQ 13: N STACKS)
 
-    Write a class, which accepts a size s and a number n, then creates n stacks out of a single list of size s.  All
-    values pushed (to any stack) must be stored in the list, however, other data structures/lists may used for other
-    purposes.  A stack is full only when the entire list (of size s) is full.
+    Given a number of stacks, n (integer), and a total available size, s (integer), implement n stacks using a (single)
+    list of size s to store all the data/values pushed to any of the n stacks.  All stacks must maintain the condition
+    that a (single) stack becomes full only when the entire list (of size s) is full.  Note that other data structures
+    (or lists) may used to aid in the stacks logistics (but not to store stack values).
 
 """
 
 
-# Fixed Division Approach: Simply divide the array into three equal parts.
-class FixedMultiStack:
-    def __init__(self, stack_size, number_of_stacks=3):
-        self.number_of_stacks = number_of_stacks
-        self.stack_capacity = stack_size
-        self.values = [None] * (self.stack_capacity * self.number_of_stacks)
-        self.sizes = [0] * self.number_of_stacks
+# Questions you should ask the interviewer (if not explicitly stated):
+#   - What data type will the stack contain?  (This may affect some languages more than others.)
+#   - What do you want to happen when a push occurs on a full stack?
+#   - What do you want to happen when a pop occurs on an empty stack?
+#   - What methods do you want implemented?  (Define the methods signatures, if time allows then add/finish logic.)
+
+
+# WRONG APPROACH: Naive Fixed Division
+#
+# Simply divide the array into three equal parts.
+#
+# NOTE: This DOES NOT maintain the condition that "a stack becomes full only when the entire list (of size s) is full"!
+class StacksViaListWrong:
+    def __init__(self, num_stacks=3, size=10):
+        if not isinstance(num_stacks, int) or not isinstance(size, int) or num_stacks < 1 or size < 1:
+            raise TypeError(f"FixedMultiStack arguments must be ints greater than zero.")
+        self.num_stacks = num_stacks
+        self.stack_capacity = size // num_stacks
+        self.values = [None] * size
+        self.sizes = [0] * self.num_stacks
 
     def __repr__(self):
-        return repr(self.values)
+        return "\n\tvalues: " + repr(self.values)
 
     def push(self, stack_num, value):
         if self.full(stack_num):
@@ -52,11 +66,17 @@ class FixedMultiStack:
         return self.sizes[stack_num] == 0
 
 
-# Flexible Divisions Approach:  Allow each stack to be flexible in size, and use the list as a circular list.
+# (COMPLEX) APPROACH:  Flexible Divisions (StacksViaListComplex)
+#
+# Allow each stack to be flexible in size, and use the list as a circular list.  Is composed of two classes:
+#   StackInfo - Contains the logistic/management information for a single stack.
+#   StacksViaListComplex - Class that contains the (single) list with all stack values as well as StackInfo objects.
 class StackInfo:
+    """This Class contains the logistic/management information for a single stack."""
+
     def __init__(self, start, capacity):
         self.start = start
-        self.capacity = capacity
+        self.capacity = capacity            # Can change.
         self.size = 0
 
     # Check if index in full list is within stack boundaries.  The stack can wrap around to the start of list.
@@ -80,18 +100,26 @@ class StackInfo:
         return self.size == 0
 
     def __repr__(self):
-        return repr({"start": self.start, "capacity": self.capacity, "size": self.size})
+        return repr({"start": self.start,
+                     "capacity": self.capacity,
+                     "size": self.size})
 
 
-class MultiStack:
-    def __init__(self, number_of_stacks, default_size):
+# (COMPLEX) APPROACH CONTINUED:  Flexible Divisions (StacksViaListComplex)
+class StacksViaListComplex:
+    def __init__(self, num_stacks, size):
+        if not isinstance(num_stacks, int) or not isinstance(size, int) or num_stacks < 1 or size < 1:
+            raise TypeError(f"MultiStack arguments must be ints greater than zero.")
         self.info = []
-        for i in range(number_of_stacks):
+        self.values = [None] * size
+        for i in range(num_stacks):
+            default_size = size // num_stacks
+            if i == 0:
+                default_size += size % num_stacks
             self.info.append(StackInfo(default_size * i, default_size))
-        self.values = [None] * (number_of_stacks * default_size)
 
     def __repr__(self):
-        return repr(self.info) + repr(self.values)
+        return "\n\tinfo: " + repr(self.info) + "\n\tvalues: " + repr(self.values)
 
     def push(self, stack_num, value):
         if self.all_stacks_are_full():
@@ -115,9 +143,9 @@ class MultiStack:
         stack = self.info[stack_num]
         return self.values[stack.last_element_index(self.adjust_index)]
 
-    # Shifts items in stack over by one element.  If available capacity, then shrink stack by one element.  If not, then
-    # shift the next stack over too.
     def shift(self, stack_num):
+        """Shifts items in stack over by one element.
+           If available capacity, then shrink stack by one element.  If not, then shift the next stack over too."""
         # print(f"/// Shifting {stack_num}")
         stack = self.info[stack_num]
         if stack.size >= stack.capacity:                                # If this stack is at full capacity:
@@ -146,8 +174,8 @@ class MultiStack:
             size += i.size
         return size
 
-    # Adjust index to be within range of 0 to len - 1.
     def adjust_index(self, index):
+        """Adjust index to be within range of 0 to len - 1."""
         return index % len(self.values)
 
     def next_index(self, index):
@@ -157,24 +185,117 @@ class MultiStack:
         return self.adjust_index(index - 1)
 
 
-fixed_multi_stacks = FixedMultiStack(5)
-for i in range(3):
-    for j in range(i + 2):
-        print(f"fixed_multi_stacks.push({i}, {str(i) + str(j)})")
-        fixed_multi_stacks.push(i, str(i) + str(j))
-print(f"fixed_multi_stacks: {fixed_multi_stacks}")
-for i in range(3):
-    print(f"fixed_multi_stacks.peek({i}): {fixed_multi_stacks.peek(i)}")
-    print(f"fixed_multi_stacks.pop({i}): {fixed_multi_stacks.pop(i)}")
-print(f"fixed_multi_stacks: {fixed_multi_stacks}")
-print()
+# (SIMPLE) APPROACH:  Via Pointers (StacksViaList)
+#
+# This approach simply interleaves the stacks data (so the stacks may be non-contagious in the list) via a minimal set
+# of pointers which track the state of the stacks/list.  The stacks are maintained via:
+#   Top Of Stack List:  Length n, where each index points to the corresponding stacks top in the list, or -1 if empty.
+#   Stack Data:         The actual list, of length s, which contains the values on the stacks.
+#   Next Index:         Length s, where indices point to either the:
+#                           Next available list index, if corresponds to a stacks top, or a unused index.
+#                           Previous (or last used) index for the corresponding stack.
+#   Next Available:     The first empty index in the list.
+#
+# SEE: Console output for printed representation of the pointers values.
+#
+# NOTE: This approach interleaves stacks values!
+class StacksViaList:
+    def __init__(self, num_stacks, capacity):
+        if not isinstance(num_stacks, int) or not isinstance(capacity, int) or num_stacks < 1 or capacity < 1:
+            raise TypeError(f"num_stacks and capacity must be ints greater than zero.")
+        self.top_of_stack = [-1 for _ in range(num_stacks)]             # Top Index (Per Stack)
+        self.stack_data = [None for _ in range(capacity)]               # Actual Data/Values
+        self.next_index = [i for i in range(1, capacity)] + [-1]        # Next (All) & Previous (Per Stack) Pointer.
+        self.next_available = 0                                         # Next Index, -1 can serve as a full flag.
 
-multi_stacks = MultiStack(3, 5)
-for i in range(3):
-    print(f"multi_stacks.push(2, {'2'+str(i)}): {multi_stacks.push(2, '2'+str(i))}")
+    def __repr__(self):
+        return "\n\ttop_of_stack: " + repr(self.top_of_stack) + "\n\tstack_data: " + repr(self.stack_data) + "\n\tnext_index: " + repr(self.next_index) + "\n\tnext_available: " + repr(self.next_available)
 
-for i in range(8):
-    print(f"multi_stacks.push(1, {'1' + str(i)}): {multi_stacks.push(1, '1' + str(i))}")
-print(f"multi_stacks: {multi_stacks}")
+    def push(self, stack, value):
+        if not isinstance(stack, int) or stack < 0 or stack >= len(self.top_of_stack):
+            raise TypeError(f"Invalid stack value.")
+        if self.next_available < 0:
+            raise TypeError(f"All stacks full.")
+        curr_index = self.next_available
+        self.next_available = self.next_index[curr_index]
+        self.stack_data[curr_index] = value
+        self.next_index[curr_index] = self.top_of_stack[stack]
+        self.top_of_stack[stack] = curr_index
+
+    def pop(self, stack):
+        if not isinstance(stack, int) or stack < 0 or stack >= len(self.top_of_stack):
+            raise TypeError(f"Invalid stack value.")
+        if self.top_of_stack[stack] < 0:
+            raise IndexError(f"Pop from empty stack.")
+        curr_index = self.top_of_stack[stack]
+        result = self.stack_data[curr_index]
+        self.stack_data[curr_index] = None      # Needed for correct __repr__ state/representation.
+        self.top_of_stack[stack] = self.next_index[curr_index]
+        self.next_index[curr_index] = self.next_available
+        self.next_available = curr_index
+        return result
+
+    def peek(self, stack):
+        if not isinstance(stack, int) or stack < 0 or stack >= len(self.top_of_stack):
+            raise TypeError(f"Invalid stack value.")
+        if self.top_of_stack[stack] < 0:
+            raise IndexError(f"Peek from empty stack.")
+        return self.stack_data[self.top_of_stack[stack]]
+
+    def is_full(self):
+        return self.next_available < 0
+
+    def is_empty(self):
+        return all([x < 0 for x in self.top_of_stack])
+
+
+classes = [StacksViaListWrong,
+           StacksViaListComplex,
+           StacksViaList]
+
+for cls in classes:
+    print(f"===== {cls.__name__} Class =====")
+    stacks = cls(3, 10)
+    print(f"stacks = {cls.__name__}(3, 10)")
+    print(f"stacks: {stacks}")
+
+    # Push and pop from the third stack:
+    print(f"stacks.push(2, 's2,v0'): {stacks.push(2, 's2,v0')}")
+    print(f"stacks: {stacks}")
+    print(f"stacks.pop(2): {stacks.pop(2)!r}")
+    print(f"stacks: {stacks}")
+
+    # Push 3 values to the first stack:
+    for i in range(3):
+        print(f"stacks.push(0, 's0,v{i}'): {stacks.push(0, 's0,v' + str(i))}")
+    print(f"stacks: {stacks}")
+
+    # Try pushing 7 values to the second stack:
+    try:
+        for i in range(7):
+            print(f"stacks.push(1, 's1,v{i}'): ", end="")
+            print(f"{stacks.push(1, 's1,v' + str(i))}")
+    except Exception as e:
+        print(f"Exception Caught: {e}")
+    print(f"stacks: {stacks}")
+
+    # Try pushing to each of the stacks.
+    try:
+        for i in range(1, 3):
+            print(f"stacks.push(2, 's1,v{i}'): ", end="")
+            print(f"{stacks.push(2, 's1,v' + str(i))}")
+    except Exception as e:
+        print(f"Exception Caught: {e}")
+    print(f"stacks: {stacks}")
+
+    # Try popping one off of each of the stacks:
+    try:
+        for i in range(3):
+            print(f"stacks.pop({i}): ", end="")
+            print(f"{stacks.pop(i)}")
+    except Exception as e:
+        print(f"Exception Caught: {e}")
+    print(f"stacks: {stacks}")
+    print("\n")
 
 
