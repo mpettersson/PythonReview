f162aa99799f18e139e8dfdcbb177a05ec1cf65b
Matthew Pettersson <matt@mpettersson.com>
2024-09-19 03:16:49 -0700

    Cleaned up perms with dups question.

--- questions/list_and_recursion/permutations_with_duplicates.py
+++ questions/list_and_recursion/permutations_with_duplicates.py
@@ -24,7 +24,7 @@ import time
 # Time Complexity: O(n!), where n is the length of the string.
 # Space Complexity: O(n!), where n is the length of the string.
 def get_itertools_perms(s):
-    if s is not None:
+    if s is not None and isinstance(s, str):
         return list(set(map(''.join, itertools.permutations(s))))
 
 
@@ -36,18 +36,22 @@ def get_itertools_perms(s):
 # Time Complexity: O(n!), where n is the length of the string.
 # Space Complexity: O(n!), where n is the length of the string.
 def get_perms_w_dups_naive(s):
-    if s is not None:
-        if len(s) <= 1:
+
+    def _rec(s):
+        if len(s) == 0:
             return [s]
-        head = s[0]
-        tail_perms = get_perms_w_dups_naive(s[1:])
+        h = s[0]                                # Head
+        t = _rec(s[1:])                         # Tail Permutations
         result = []
-        for l in tail_perms:
+        for ps in t:                            # For each permutation string (recursive results)...
             for i in range(len(s)):
-                candidate = l[0:i] + head + l[i:len(l)]
+                candidate = ps[:i] + h + ps[i:]
                 if candidate not in result:
                     result.append(candidate)
         return result
+
+    if s is not None and isinstance(s, str):
+        return _rec(s)
 
 
 # APPROACH: Naive Permutation Via Set
@@ -58,26 +62,30 @@ def get_perms_w_dups_naive(s):
 # Time Complexity: O(n!), where n is the length of the string.
 # Space Complexity: O(n!), where n is the length of the string.
 #
-# NOTE: Although this solution is 'naive' (not as cleaver as the dictionary approach below), in many instances it is
-# faster than the other approaches.
+# NOTE: Although this solution is 'naive' (not as cleaver as the dictionary approach below), in many instances (i.e.,
+# when there are few duplicates) it is faster than the other approaches.
 def get_perms_w_dups_via_set(s):
-    if s is not None:
-        if len(s) <= 1:
+
+    def _rec(s):
+        if len(s) == 0:
             return [s]
-        head = s[0]
-        tail_perms = get_perms_w_dups_via_set(s[1:])
+        h = s[0]                                # Head
+        t = _rec(s[1:])                         # Tail Permutations
         result = set()
-        for l in tail_perms:
+        for ps in t:                            # For each permutation string (recursive results)...
             for i in range(len(s)):
-                result.add(l[0:i] + head + l[i:len(l)])
+                result.add(ps[:i] + h + ps[i:])
         return list(result)
+
+    if s is not None and isinstance(s, str):
+        return _rec(s)
 
 
 # APPROACH: Via Dictionary
 #
 # This solution uses a dict, which contains each char and the number of times it appears, to only create unique
-# permutations (rather than creating duplicate permutations) via building a prefix that is appended to the results when
-# the index (which is initialized equal to the length of the string) reaches zero.
+# permutations (rather than creating duplicate permutations) via building an accumulator that is appended to the results
+# when the index (which is initialized equal to the length of the string) reaches zero.
 #
 # For example, given the strings 'ABCDE' and 'AABBC':
 #       Permutations('ABCDE') == 5! / (1! * 1! * 1! * 1! * 1!) == 120
@@ -94,14 +102,14 @@ def get_perms_w_dups_via_set(s):
 # Space Complexity: O(n!/(num_c_1! * num_c_2! * ... * num_c_m!), where n = len(s), and num_c is the unique char count.
 def get_perms_w_dups_via_dict(s):
 
-    def _rec(d, i, prefix, result):
+    def _rec(i, accumulator):
         if i == 0:
-            result.append(prefix)
+            result.append(accumulator)
         else:
             for k in d.keys():
                 if d[k] > 0:
                     d[k] -= 1
-                    _rec(d, i - 1, prefix + k, result)
+                    _rec(i-1, accumulator + k)
                     d[k] += 1
 
     if s is not None:
@@ -109,35 +117,33 @@ def get_perms_w_dups_via_dict(s):
         d = {}
         for c in s:
             d[c] = d.setdefault(c, 0) + 1
-        _rec(d, len(s), "", result)
+        _rec(len(s), "")
         return result
 
 
-# NOTE: The following is the dictionary approach (above) implemented for lists.
+# NOTE: The following is the "Via Dictionary" approach (see above) implemented for lists.
 def get_list_perms_w_dups_via_dict(l):
 
-    def _rec(d, i, a, result):
+    def _rec(i, accumulator):
         if i == 0:
-            result.append(a[:])
+            result.append(accumulator)
         else:
             for k in d:
                 if d[k] > 0:
                     d[k] -= 1
-                    a.append(k)
-                    _rec(d, i - 1, a, result)
-                    a.pop()
+                    _rec(i-1, accumulator + [k])
                     d[k] += 1
 
-    if l is not None:
+    if l is not None and isinstance(l, list):
         result = []
         d = {}
         for c in l:
             d[c] = d.setdefault(c, 0) + 1
-        _rec(d, len(l), [], result)
+        _rec(len(l), [])
         return result
 
 
-args = ["", "A", "AB", "ABC", "AAB", "AAAB", "AAAAB", "AABBC"]
+args = ["", "A", "AB", "ABC", "AAB", "AAAB", "AAAAB", "AABBC", "AAAAAA"]
 fns = [get_itertools_perms,
        get_perms_w_dups_naive,
        get_perms_w_dups_via_set,
@@ -146,14 +152,17 @@ fns = [get_itertools_perms,
 for s in args:
     for fn in fns:
         print(f"{fn.__name__}({repr(s)}): {fn(s)}")
+    print(f"get_list_perms_w_dups_via_dict({repr(s)}): ", list(map(''.join, get_list_perms_w_dups_via_dict(list(s)))))
     print()
-
 
 s = "ABBCCCDDDD"
 for fn in fns:
-    t = time.time()
+    st = time.time()
     print(f"{fn.__name__}({s!r})", end="")
     fn(s)
-    print(f" took {time.time() - t} seconds")
-
+    print(f" took {time.time() - st} seconds")
+st = time.time()
+print(f"get_list_perms_w_dups_via_dict({repr(s)})", end="")
+list(map(''.join, get_list_perms_w_dups_via_dict(list(s))))
+print(f" took {time.time() - st} seconds")
 
