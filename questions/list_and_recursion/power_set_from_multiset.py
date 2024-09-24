001d44e528e23dd05ed62af2f38eb27a041c4b07
Matthew Pettersson <matt@mpettersson.com>
2024-09-23 23:24:23 -0700

    Updated power set from multiset question.

--- questions/list_and_recursion/power_set_from_multiset.py
+++ questions/list_and_recursion/power_set_from_multiset.py
@@ -10,7 +10,7 @@
         From Wikipedia; "in mathematics, the power set (or powerset) of any set S is the set of all subsets of S,
         including the empty set and S itself".  SEE: wikipedia.org/wiki/Power_set
 
-        Also from Wikipedia; "in mathematics, a multiset (or bag, or mset) is a modification of the concept of a set
+        Also, from Wikipedia; "in mathematics, a multiset (or bag, or mset) is a modification of the concept of a set
         that, unlike a set, allows for multiple instances for each of its elements."  SEE: wikipedia.org/wiki/Multiset
 
     Example:
@@ -40,9 +40,7 @@ import time
 #       not including) each of the elements of the set.  Therefore, the size of the set is the number of combinations p,
 #       or p = n**r, where n is the number of possible items (2--either include or exclude) and r is the number of items
 #       in the set.
-
-
-# NOTE: The following are listed in order of fastest to slowest (for an input with length 18):
+# NOTE: Lexicographical order (sorting) is key to this question...
 
 
 # APPROACH: Itertools Combinations
@@ -53,12 +51,13 @@ import time
 # Time Complexity: O(2**n), where n is the number of list items.
 # Space Complexity: O(2**n), where n is the number of list items.
 def power_set_from_multiset_itertools_combinations(iterable):
-    result = set()
-    l = sorted(iterable)
-    for r in range(len(l) + 1):
-        for e in itertools.combinations(l, r):
-            result.add(tuple(e))
-    return result                       # return [list(e) for e in result] for a list of lists...
+    if iterable is not None and hasattr(iterable, '__iter__'):
+        result = set()
+        l = sorted(iterable)
+        for r in range(len(l) + 1):
+            for e in itertools.combinations(l, r):
+                result.add(tuple(e))
+        return result
 
 
 # APPROACH: (Minimized) Itertools Combinations
@@ -70,7 +69,31 @@ def power_set_from_multiset_itertools_combinations(iterable):
 #
 # NOTE: This is a close second to the fastest approach.
 def power_set_from_multiset_itertools_combinations_min(iterable):
-    return {tuple(comb) for r in range(len(iterable) + 1) for comb in itertools.combinations(sorted(iterable), r)}
+    if iterable is not None and hasattr(iterable, '__iter__'):
+        return {tuple(comb) for r in range(len(iterable)+1) for comb in itertools.combinations(sorted(iterable), r)}
+
+
+# APPROACH: DFS/Recursive
+#
+# This is simply a recursive DFS traversal of the elements of the iterable (which are converted to a list for
+# processing), where the value for each 'node' (in the tree) is added to the results list.
+#
+# Time Complexity: O(2**n), where n is the number of list items.
+# Space Complexity: O(2**n), where n is the number of list items.
+def power_set_from_multiset_rec_list(iterable):
+
+    def _rec(i, path):
+        result.append(path)
+        for j in range(i, len(l)):
+            if j > i and l[j] == l[j-1]:
+                continue
+            _rec(j+1, path+[l[j]])
+
+    if iterable is not None and hasattr(iterable, '__iter__'):
+        result = []
+        l = sorted(iterable)
+        _rec(0, [])
+        return result
 
 
 # APPROACH: Recursive
@@ -81,19 +104,20 @@ def power_set_from_multiset_itertools_combinations_min(iterable):
 #
 # Time Complexity: O(2**n), where n is the number of list items.
 # Space Complexity: O(2**n), where n is the number of list items.
-def power_set_from_multiset(iterable):
+def power_set_from_multiset_rec_set(iterable):
 
     def _rec(s):
         if s is not None:
             if len(s) == 0:
-                return set(tuple())
+                return {tuple()}
             if len(s) == 1:
                 return {tuple(s), tuple()}
             h = s.pop()
             t = _rec(s)
             return t | {(*tup, h) for tup in t}
 
-    return _rec(sorted(iterable))               # return [list(e) for e in _rec(sorted(iterable))] for a list of lists
+    if iterable is not None and hasattr(iterable, '__iter__'):
+        return _rec(sorted(iterable))
 
 
 # APPROACH: Reduce/Fold Left & Lambda
@@ -104,29 +128,8 @@ def power_set_from_multiset(iterable):
 # Time Complexity: O(2**n), where n is the number of list items.
 # Space Complexity: O(2**n), where n is the number of list items.
 def power_set_from_multiset_functools_reduce(iterable):
-    return functools.reduce(lambda tail, head: tail | {(*tup, head) for tup in tail}, sorted(iterable), {tuple()})
-
-
-# APPROACH: DFS/Recursive
-#
-# This is simply a recursive DFS traversal of the elements of the iterable (which are converted to a list for
-# processing), where the value for each 'node' (in the tree) is added to the results list.
-#
-# Time Complexity: O(2**n), where n is the number of list items.
-# Space Complexity: O(2**n), where n is the number of list items.
-def power_set_from_multiset_dfs(iterable):
-
-    def _dfs(i, path):
-        result.append(path)
-        for j in range(i, len(l)):
-            if j > i and l[j] == l[j-1]:
-                continue
-            _dfs(j+1, path+[l[j]])
-
-    result = []
-    l = sorted(iterable)
-    _dfs(0, [])
-    return result                       # return [list(e) for e in result] for a list of lists...
+    if iterable is not None and hasattr(iterable, '__iter__'):
+        return functools.reduce(lambda tail, head: tail | {(*tup, head) for tup in tail}, sorted(iterable), {tuple()})
 
 
 # APPROACH: Combinatorics/Iterative/Bit Masking:
@@ -137,38 +140,68 @@ def power_set_from_multiset_dfs(iterable):
 #
 # Time Complexity: O(2**n), where n is the number of list items.
 # Space Complexity: O(2**n), where n is the number of list items.
-def power_set_from_multiset_combinatorics(s):
-    if s is not None:
+def power_set_from_multiset_combinatorics(iterable):
+    if iterable is not None and hasattr(iterable, '__iter__'):
         result = set()
-        length = len(s)
-        l = sorted(s)
-        for i in range(2**length):
-            result.add(tuple([x for b, x in zip([int(c) for c in f"{i:b}".zfill(length)], l) if b]))
-        return result                   # return [list(e) for e in result] for a list of lists...
+        l = sorted(iterable)
+        for i in range(2**len(iterable)):
+            result.add(tuple([x for b, x in zip([int(c) for c in f"{i:b}".zfill(len(iterable))], l) if b]))
+        return result
 
 
-iterables = [['B', 'A', 'B'],
+# APPROACH: Via Dictionary
+#
+# This is simply a recursive DFS traversal of the elements of the iterable (which are converted to a list for
+# processing), where the value for each 'node' (in the tree) is added to the results list.
+#
+# Time Complexity: O(2**n), where n is the number of list items.
+# Space Complexity: O(2**n), where n is the number of list items.
+def power_set_from_multiset_via_dict(iterable):
+
+    def _rec(i, accumulator):
+        result.append(accumulator)
+        for k in d.keys():
+            if d[k] > 0:
+                d[k] -= 1
+                if len(accumulator) == 0 or accumulator[-1] <= k:
+                    _rec(i-1, accumulator + [k])
+                d[k] += 1
+
+    if iterable is not None and hasattr(iterable, '__iter__'):
+        result = []
+        d = dict()
+        for k in iterable:
+            d[k] = d.setdefault(k, 0)+1
+        _rec(len(iterable), [])
+        return result
+
+
+iterables = [{'A', 'B', 'C'},
+             ['B', 'A', 'B'],
+             ['B', 'A', 'B', 'A'],
+             ['A', 'A', 'A', 'A'],
              [2, 1, 2, 0],
              "aba",
-             []]
+             [],
+             None]
 fns = [power_set_from_multiset_itertools_combinations,
        power_set_from_multiset_itertools_combinations_min,
-       power_set_from_multiset,
+       power_set_from_multiset_rec_list,
+       power_set_from_multiset_rec_set,
        power_set_from_multiset_functools_reduce,
-       power_set_from_multiset_dfs,
-       power_set_from_multiset_combinatorics]
+       power_set_from_multiset_combinatorics,
+       power_set_from_multiset_via_dict]
 
 for iterable in iterables:
     print(f"iterable: {iterable}")
     for fn in fns:
-        print(f"{fn.__name__}(iterable): {fn(iterable)}")
+        print(f"{fn.__name__}(iterable): {len(fn(iterable)) if iterable is not None else 0}:{fn(iterable)}")
     print()
 
-s = set(range(18))
+s = set(range(19))
 print(f"s: {s}")
 for fn in fns:
     t = time.time(); print(f"{fn.__name__}(s)", end="")
     fn(set(s))
     print(f" took {time.time() - t} seconds")
-
 
