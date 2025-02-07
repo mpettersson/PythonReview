337ef4212611c6fe250c4119949215541ee70711
Matthew Pettersson <matt@mpettersson.com>
2025-02-06 22:43:39 -0800

    Starting to update 8 queens problem.

--- questions/list_and_recursion/eight_queens.py
+++ questions/list_and_recursion/eight_queens.py
@@ -1,17 +1,38 @@
 """
-    EIGHT QUEENS (CCI 8.12)
+    N QUEENS (CCI 8.12: EIGHT QUEENS,
+              leetcode.com/problems/n-queens)
 
+    TODO: Rewrite prompt.
     Write an algorithm to print all ways of arranging eight queens on an 8x8 chess board so that none of them share the
     same row, column, or diagonal. In this case, "diagonal" means all diagonals, not just the two that bisect the board.
+
+    Example:
+        Input = TODO
+        Output = TODO
+
+    Variations:
+        - TODO
+
+    SEE: https://sites.google.com/site/nqueensolver/home/algorithm-results for an interesting comparison of approaches.
 """
 import copy
 import itertools
 
-# SEE: https://sites.google.com/site/nqueensolver/home/algorithm-results for an interesting comparison of approaches.
+
+# Questions you should ask the interviewer (if not explicitly stated):
+#   - What time/space complexity are you looking for?
+#   - TODO
 
 
 # Naive/Brute Force List Approach:  Get all permutations of a list representation of the queens arrangements, then check
 # and remove invalid arrangements.
+
+# APPROACH: TODO
+#
+# TODO
+#
+# Time Complexity: TODO
+# Space Complexity: TODO
 def arrange_queens_via_list_naive(size=8):
 
     def _check_arrangement(l):
@@ -30,17 +51,22 @@ def arrange_queens_via_list_naive(size=8):
         else:
             results.pop(i)
 
-    if len(results) is 0:
-        print(f"No possible queen arrangement for {size}x{size} board.")
-    else:
-        for i, result in enumerate(results):
-            print(f"Arrangement {i}:\n{result}\n")
+    if len(results) == 0:
+        return f"No possible queen arrangement for {size}x{size} board."
+    return results
 
 
 # Recursive/Backtracking Matrix Approach:  Try to arrange a queen at each column on a row, if the attempt is a valid
 # placement, then recurse to the next row.  When a series of recursive attempts has all queens arranged, the arrangement
 # is added to the results and control 'backtracks' to a previous row (stack state) to continue placements (and possibly
 # more recursive calls).
+
+# APPROACH: TODO
+#
+# TODO
+#
+# Time Complexity: TODO
+# Space Complexity: TODO
 def arrange_queens_via_matrix(size=8):
 
     def _check_diagonal(r, c, m):
@@ -73,15 +99,20 @@ def arrange_queens_via_matrix(size=8):
     if size is not None and 0 < size:
         results = []
         _arrange_queens_via_matrix([], results, size)
-        if len(results) is 0:
-            print(f"No possible queen arrangement for {size}x{size} board.")
-        else:
-            for i, result in enumerate(results):
-                print(f"Arrangement {i}:\n{result}\n")
+        if len(results) == 0:
+            return f"No possible queen arrangement for {size}x{size} board."
+        return results
 
 
 # Recursive/Backtracking List Approach:  Similar to the above matrix approach, however, just using a list to represent
 # the queens position in a matrix.
+
+# APPROACH: TODO
+#
+# TODO
+#
+# Time Complexity: TODO
+# Space Complexity: TODO
 def arrange_queens_via_list(size=8):
 
     def _check_valid(queens, curr_row, curr_col):
@@ -108,21 +139,55 @@ def arrange_queens_via_list(size=8):
         results = []
         queens = [0] * size
         _arrange_queens_via_list(size, 0, queens, results)
-        if len(results) is 0:
-            print(f"No possible queen arrangement for {size}x{size} board.")
+        if len(results) == 0:
+            return f"No possible queen arrangement for {size}x{size} board."
+        return results
+
+
+# APPROACH: TODO
+#
+# TODO
+#
+# Time Complexity: TODO
+# Space Complexity: TODO
+def n_queens_backtrack(n):
+
+    def _rec(r):        # Recursive Backtracking
+        if r == n:
+            result.append(["".join(row) for row in board])
         else:
-            for i, result in enumerate(results):
-                print(f"Arrangement {i}:\n{result}\n")
+            for c in range(n):
+                if c in col or (r+c) in posDiag or (r-c) in negDiag:
+                    continue
+                col.add(c)
+                posDiag.add(r+c)
+                negDiag.add(r-c)
+                board[r][c] = "Q"
+                _rec(r+1)
+                col.remove(c)
+                posDiag.remove(r+c)
+                negDiag.remove(r-c)
+                board[r][c] = "."
+
+    col = set()
+    posDiag = set()     # (r+c)
+    negDiag = set()     # (r-c)
+    result = []
+    board = [["."] * n for _ in range(n)]
+    _rec(0)
+    return result
 
 
-n = 8
-print(f"arrange_queens_via_list_naive({n}):")
-arrange_queens_via_list_naive(n)
+n_vals = [0, 1, 2, 3, 4, 8]
+fns = [arrange_queens_via_list_naive,
+       arrange_queens_via_matrix,
+       arrange_queens_via_list,
+       n_queens_backtrack]
 
-print(f"arrange_queens_via_matrix({n}):")
-arrange_queens_via_matrix(n)
+for n in n_vals:
+    for fn in fns:
+        print(f"{fn.__name__}({n}): {fn(n)}")
+    print()
 
-print(f"arrange_queens_via_list({n}):")
-arrange_queens_via_list(n)
 
 
