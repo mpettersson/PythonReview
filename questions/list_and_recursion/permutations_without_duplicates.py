71470f9890ab7e027b82a30576bcf1a61cc2f6a5
Matthew Pettersson <matt@mpettersson.com>
2025-02-20 16:33:18 -0800

    Added more diverse approaches, removed some not so interesting approaches.

--- questions/list_and_recursion/permutations_without_duplicates.py
+++ questions/list_and_recursion/permutations_without_duplicates.py
@@ -1,6 +1,7 @@
 """
     PERMUTATIONS WITHOUT DUPLICATES (CCI 8.7: PERMUTATIONS WITHOUT DUPS,
-                                     50CIQ 12: PERMUTATIONS)
+                                     50CIQ 12: PERMUTATIONS,
+                                     leetcode.com/problems/permutations/)
 
     Write a function, which accepts a string of unique characters, and returns a list of all the strings permutations.
 
@@ -18,13 +19,14 @@ import time
 #   - What is the argument type?  (This may affect some languages more than others.)
 #   - Are there duplicate elements?
 
+
 # APPROACH: Via Itertools Permutations
 #
 # This approach simply uses Python's itertools.permutations method.
 #
 # Time Complexity: O(n!), where n is the length of the string.
 # Space Complexity: O(n!), where n is the length of the string.
-def get_itertools_perms(s):
+def get_perms_wo_dups_itertools(s):
     if isinstance(s, str):
         return list(map(''.join, itertools.permutations(s)))
 
@@ -37,46 +39,41 @@ def get_itertools_perms(s):
 #
 # Time Complexity: O(n!), where n is the length of the string.
 # Space Complexity: O(n!), where n is the length of the string.
-def get_perms_wo_dups_ht(s):
-    if s is not None:
-        n = len(s)
-        if n <= 1:                                              # Base Case: If s is empty, or a single element:
-            return [s]                                              # Return it in a list.
+def get_perms_wo_dups_rec_0(s):
+
+    def _rec(s):
+        if len(s) == 0:                             # Base Case: If s is empty, or a single element:
+            return [s]                                  # Return it in a list.
         result = []
-        for t_perm in get_perms_wo_dups_ht(s[1:]):              # For each list in the recursive result:
-            for i in range(n):                                      # In each index/position:
-                result.append(t_perm[:i] + s[0] + t_perm[i:])           # Insert the first char (head)
+        for p in _rec(s[1:]):                       # For each permutation in the recursive results:
+            for i in range(len(s)):                     # In each index/position:
+                result.append(p[:i] + s[0] + p[i:])         # Insert the first char (head).
         return result
 
-
-# APPROACH: Minimised
-#
-# Same as above, but minimized.
-#
-# Time Complexity: O(n!), where n is the length of the string.
-# Space Complexity: O(n!), where n is the length of the string.
-def get_perms_wo_dups_ht_min(s):
-    if isinstance(s, str):
-        l = len(s)
-        return [s] if l <= 1 else [t_perm[:i] + s[0] + t_perm[i:] for t_perm in get_perms_wo_dups_ht_min(s[1:]) for i in range(l)]
+    if s is not None and isinstance(s, str):
+        return _rec(s)
 
 
 # APPROACH: Build From All Character Substring
 #
-# For each character in the string, recurse on the the remainder of the string, then add the (missing) character to each
+# For each character in the string, recurse on the remainder of the string, then add the (missing) character to each
 # permutation in the results of the recursive call.
 #
 # Time Complexity: O(n!), where n is the length of the string.
 # Space Complexity: O(n!), where n is the length of the string.
-def get_perms_wo_dubs(s):
-    if s is not None:
+def get_perms_wo_dups_rec_1(s):
+
+    def _rec(s):
         if len(s) == 0:
             return [""]
         result = []
-        for i in range(len(s)):                                 # For each character in the string,
-            for p in get_perms_wo_dubs(s[:i] + s[i + 1:]):          # For each permutation of the string (w/o the char),
-                result.append(s[i] + p)                                 # Add the character to the head of each list.
+        for i in range(len(s)):                     # For each character in the string,
+            for p in _rec(s[:i] + s[i+1:]):             # For each permutation of the string (w/o the char),
+                result.append(s[i] + p)                     # Add the character to the head of each list.
         return result
+
+    if s is not None and isinstance(s, str):
+        return _rec(s)
 
 
 # APPROACH: Push The 'Prefix' Down The Stack
@@ -87,27 +84,82 @@ def get_perms_wo_dubs(s):
 #
 # Time Complexity: O(n!), where n is the length of the string.
 # Space Complexity: O(n!), where n is the length of the string.
-def get_perms_wo_dubs_prefix(s):
+def get_perms_wo_dups_rec_2(s):
 
-    def _get_perms_wo_dubs_prefix(prefix, remainder, perms):
+    def _rec(prefix, remainder):
         if len(remainder) == 0:                                 # Once the remainder is empty,
-            perms.append(prefix)                                # Add the prefix/valid permutation.
+            result.append(prefix)                                # Add the prefix/valid permutation.
         else:
             for i in range(len(remainder)):                     # Else, for each index, add it to the prefix and recurse
-                _get_perms_wo_dubs_prefix(prefix + remainder[i], remainder[:i] + remainder[i + 1:], perms)
+                _rec(prefix + remainder[i], remainder[:i] + remainder[i + 1:])
 
     if s is not None:
-        perms = []
-        _get_perms_wo_dubs_prefix("", s, perms)                  # Note the empty string accumulator.
-        return perms
+        result = []
+        _rec("", s)                  # Note the empty string accumulator.
+        return result
+
+
+# APPROACH: Recursive with Index Pointer
+#
+# Yet another recursive approach, similar to the approaches above, with one difference; this approach uses a pointer
+# with the original string. The approaches above pass slices (new strings) and therefore operate with a higher overhead.
+#
+# Time Complexity: O(n!), where n is the length of the string.
+# Space Complexity: O(n!), where n is the length of the string.
+def get_perms_wo_dups_rec(s):
+
+    def _rec(s, i):
+        if i == len(s):
+            return [""]
+        h = s[i]
+        t = _rec(s, i+1)
+        results = []
+        for res in t:
+            for j in range(len(res)+1):
+                results.append(res[:j] + h + res[j:])
+        return results
+
+    if s is not None and isinstance(s, str):
+        return _rec(s, 0)
+
+
+# APPROACH: Via Dictionary
+#
+# This solution uses a dict, which contains each char and the number of times it appears, to create permutations via
+# building an accumulator that is appended to the results when the index (which is initialized equal to the length of
+# the string) reaches zero.  This, although slower than the optimal solution above, it also handles question variations
+# with duplicate characters in the string...
+#
+# Time Complexity: O(n!), where n is the length of the string (and there are NO duplicates).
+# Space Complexity: O(n!), where n is the length of the string (and there are NO duplicates).
+def get_perms_wo_dups_dict(s):
+
+    def _rec(i, accumulator):
+        if i == 0:
+            result.append(accumulator)
+        else:
+            for k in d.keys():
+                if d[k] > 0:
+                    d[k] -= 1
+                    _rec(i-1, accumulator + k)
+                    d[k] += 1
+
+    if s is not None:
+        result = []
+        d = {}
+        for c in s:
+            d[c] = d.setdefault(c, 0) + 1
+        _rec(len(s), "")
+        return result
 
 
 args = ["A", "AB", "ABC", "1234", "", None]
-fns = [get_itertools_perms,
-       get_perms_wo_dups_ht,
-       get_perms_wo_dups_ht_min,
-       get_perms_wo_dubs,
-       get_perms_wo_dubs_prefix]
+fns = [get_perms_wo_dups_itertools,
+       get_perms_wo_dups_rec_0,
+       get_perms_wo_dups_rec_1,
+       get_perms_wo_dups_rec_2,
+       get_perms_wo_dups_rec,
+       get_perms_wo_dups_dict]
 
 for s in args:
     for fn in fns:
@@ -115,7 +167,7 @@ for s in args:
     print()
 
 
-s = "ABCDEFGHIJ"
+s = "ABCDEFGHIJL"
 for fn in fns:
     t = time.time()
     print(f"{fn.__name__}({s!r})", end="")
